6e48b410b4061011916c15e2720e1a4786e9f81f
= <mike.shiner@hotmail.com>
2017-12-08 14:28:49 +0000

    Transaction component started
    Started with transactions component, duplicating the month-view control.
    This needs to be refactored into it's own component

--- UI/src/app/transactions/transactions.component.ts
+++ UI/src/app/transactions/transactions.component.ts
@@ -1,12 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiClientService } from '../services/apiclient.service';
+
 @Component({
     selector: 'transactions-cmp',
     moduleId: module.id,
     templateUrl: 'transactions.component.html'
 })
 
-export class TransactionsComponent implements OnInit{
-    ngOnInit(){
+export class TransactionsComponent implements OnInit {
+    private currentViewingDate: Date;
+    private currentViewingDateString: string;
+    private getMonthText: Array<string> = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+    constructor(private apiClientService: ApiClientService) {
+        this.currentViewingDate = new Date();
+        this.updateDateString();
     }
+
+    ngOnInit() {
+
+    }
+
+    private moveMonth(direction: string): void {
+        if (direction == "next") {
+            this.currentViewingDate.setMonth(this.currentViewingDate.getMonth() + 1);
+        } else {
+            this.currentViewingDate.setMonth(this.currentViewingDate.getMonth() - 1);
+        }
+        this.refreshTransactionList();
+    }
+
+    refreshTransactionList() {
+        this.updateDateString();
+        let month = this.currentViewingDate.getMonth() + 1;
+        let year = this.currentViewingDate.getFullYear();
+    }
+
+    private updateDateString() {
+        this.currentViewingDateString = this.getMonthText[this.currentViewingDate.getMonth()] + "   '" 
+        + this.currentViewingDate.getFullYear().toString().substring(2,4);
+      }
 }
--- fullDeployment.sh
+++ fullDeployment.sh
@@ -16,7 +16,6 @@ cd ../UI
 # Build Angular frontend
 sudo ng build --env=prod
 # Build HttpServer
-cd webserver
 tsc httpServer.ts
 # Remove any running instances of webserver.js
 sudo kill $(ps aux | grep 'httpServer.js' | awk '{print $2}')
